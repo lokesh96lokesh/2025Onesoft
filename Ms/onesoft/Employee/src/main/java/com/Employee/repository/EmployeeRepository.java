b23df1db6999c4df47a62cf57eb725f2ec63e1fd
lokesh <112629714+lokesh96lokesh@users.noreply.github.com>
2025-03-26 12:01:08 +0530

    Delete Ms/onesoft/Employee directory

--- Ms/onesoft/Employee/src/main/java/com/Employee/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Employee.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Employee.Model.EmployeeModel;
-
-public interface EmployeeRepository extends JpaRepository <EmployeeModel,Integer>{
-
-}
