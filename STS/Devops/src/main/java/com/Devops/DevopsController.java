3ad60292fc50dc30ca1b2e04f20ab0028cb6c671
lokesh <112629714+lokesh96lokesh@users.noreply.github.com>
2025-03-31 09:30:27 +0530

    Add files via upload

---
+++ STS/Devops/src/main/java/com/Devops/DevopsController.java
@@ -0,0 +1,17 @@
+package com.Devops;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+public class DevopsController {
+	
+	@GetMapping("/get")
+	public String getWord(@PathVariable String a) {
+		return "Hi loki ran devops automation successfully";
+	}
+	
+
+}
