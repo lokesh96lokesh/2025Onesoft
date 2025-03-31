5454bf07fb7acc240426a4c4b037928033aeec32
lokesh <112629714+lokesh96lokesh@users.noreply.github.com>
2025-03-31 09:29:44 +0530

    Add files via upload

---
+++ DevJen/src/test/java/com/DevJen/DevJenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevJen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevJenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
